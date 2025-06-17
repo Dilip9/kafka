6992d4b3ca84466683d59a8948e2187be94ddcf1
Dilip9 <dilip.kumar-verma@capgemini.com>
2025-06-17 11:21:34 +0530

    adding logger

--- src/main/java/edu/kfk/gouri/controller/UsersController.java
+++ src/main/java/edu/kfk/gouri/controller/UsersController.java
@@ -3,6 +3,7 @@ package edu.kfk.gouri.controller;
 
 import edu.kfk.gouri.model.UserRequest;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 @CrossOrigin("*")
 @AllArgsConstructor
+@Slf4j
 public class UsersController {
 
     @PostMapping("/createUser")
     public ResponseEntity<?> createUser(@Validated @RequestBody UserRequest users){
+        log.info("Create user");
+        log.info("User details: {}", users);
         return ResponseEntity.ok(users);
 
 
