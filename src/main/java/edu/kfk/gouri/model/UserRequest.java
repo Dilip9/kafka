c8641831f5eff052055f97e3cc9f9a7870d3a3d0
Dilip9 <dilip.verma940805@gmail.com>
2025-06-25 14:25:34 +0530

    Updating few changes for common goal of security

--- src/main/java/edu/kfk/gouri/model/UserRequest.java
+++ src/main/java/edu/kfk/gouri/model/UserRequest.java
@@ -1,17 +1,39 @@
 package edu.kfk.gouri.model;
 
-import jakarta.persistence.Embedded;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import lombok.Builder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
 
 @Builder
+@Setter
+@Getter
+@ToString
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_request", uniqueConstraints = @UniqueConstraint(columnNames = "username"), indexes = {
+        @Index(name = "idx_user_request_username", columnList = "username"),
+        @Index(name = "idx_user_request_email", columnList = "email"),
+        @Index(name = "idx_user_request_phone_number", columnList = "phone_number")
+})
 public class UserRequest {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
     private String username;
 
-    @Email(message = "Invalid email format", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
-    private Email email;
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be blank")
+    private String email;
 
+    @Pattern(regexp = "^[0-9]{10}$", message = "Password must be between 8 and 20 characters and contain only alphanumeric characters")
     private String phoneNumber;
 
     private String department;
