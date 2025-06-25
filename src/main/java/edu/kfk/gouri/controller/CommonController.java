c42713611475de3c6fc76079fcebd61edc907c51
Dilip9 <dilip.verma940805@gmail.com>
2025-06-25 14:24:25 +0530

    Updating few changes for common goal of security

--- src/main/java/edu/kfk/gouri/controller/CommonController.java
+++ src/main/java/edu/kfk/gouri/controller/CommonController.java
@@ -1,4 +1,34 @@
 package edu.kfk.gouri.controller;
 
+import edu.kfk.gouri.model.GuestUserRequest;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@AllArgsConstructor
+@RequestMapping("/common/")
+@RestController
+@CrossOrigin("*")
 public class CommonController {
+
+    @GetMapping(value = "/guestUserData", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getGuestUserData(){
+        return ResponseEntity.ok("Guest User Data");
+    }
+
+    @PostMapping(value = "/createGuestUser", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createGuestUser(@Valid @RequestBody GuestUserRequest guestUserRequest) {
+        log.info("Creating guest user with data: {}", guestUserRequest);
+        // Logic to create a guest user
+        return ResponseEntity.ok("Guest user created successfully");
+
+    }
+
+
+
 }
