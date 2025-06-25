4b18711332f690d5184992404ba4c3c9bc7792f5
Dilip9 <dilip.verma940805@gmail.com>
2025-06-25 14:25:14 +0530

    Updating few changes for common goal of security

--- src/main/java/edu/kfk/gouri/model/GuestUserRequest.java
+++ src/main/java/edu/kfk/gouri/model/GuestUserRequest.java
@@ -1,4 +1,35 @@
 package edu.kfk.gouri.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "guest_user_request", indexes = {
+        @Index(name = "idx_guest_user_request_email", columnList = "email"),
+        @Index(name = "idx_guest_user_request_username", columnList = "username"),
+        @Index(name = "idx_guest_user_request_ip_address", columnList = "ipAddress")})
 public class GuestUserRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String ipAddress;
+
+    @Column(name = "country", nullable = true, length = 100)
+    private String country;
 }
